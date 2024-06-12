1284e2ea39742a6a88f1060b5304e0c689353496
Amos M. Abdallah <132259530+Amos-codes-cloud@users.noreply.github.com>
2024-06-12 23:04:52 +0300

    Add files via upload

---
+++ adminpage.php
@@ -0,0 +1,138 @@
+
+<?php
+
+
+include 'header.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		<link rel="stylesheet" href="simple-bar.css">
+		<style>
+            body {
+                background-image: url(img/bg.jpg);
+                background-size: cover;
+            }
+        </style>
+	</title>
+</head>
+
+<body>
+
+	<div class="col-md-12">
+		<div class="panel panel-success">
+			<div class="panel-heading">
+				<center><h3 >ARDHI UNIVERSITY </h3></center>
+				<center><h4> FOOD MENU AND PRICING PORTAL</h4></center>
+			</div>
+			<div class="panel-body">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"> <a href="gallery.html">
+									<span class="glyphicon glyphicon" aria-hidden="true"></span> Gallery</a>
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"> <a href="admin1.php">
+									<span class="glyphicon glyphicon" aria-hidden="true"></span>New Restaurant</a>
+								
+								</h3>
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"> <a href="admin.php">
+									<span class="glyphicon glyphicon" aria-hidden="true"></span>New Menu</a>
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"> <a href="addoffer.php">
+									<span class="glyphicon glyphicon" aria-hidden="true"></span>Add Offer </a>
+							</div>
+							
+						</div>
+					</div>
+
+
+					<div class="col-md-3">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"> <a href="userpage.php">
+									<span class="glyphicon glyphicon" aria-hidden="true"></span> View Userpage</a>
+							</div>
+							
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"> <a href="manage.php">
+									<span class="glyphicon glyphicon" aria-hidden="true"></span> Manage Users </a>
+							</div>
+							
+						</div>
+					</div>
+					
+					<div class="col-md-3">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"> <a href="offers.php">
+									<span class="glyphicon glyphicon" aria-hidden="true"></span> View Offers </a>
+							</div>
+							
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"> <a href="comments.php">
+									<span class="glyphicon glyphicon" aria-hidden="true"></span> View Coments</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"> <a href="logout.php">
+									<span class="glyphicon glyphicon" aria-hidden="true"></span> Logout</a>
+							</div>
+						</div>
+					</div>
+				
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	</body>
+	
+</html>
+
+
+
+<?php
+
+include "footer.html";
+?>
---
+++ index.php
@@ -0,0 +1,165 @@
+
+<?php
+ 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Food Menu and Pricing Portal | Ardhi
+    
+  </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/POLICE.jpg" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+  <style>
+    body{
+      background-image: url('images/background.jpg');
+    }
+    #hero{
+      background-image: url('img/food5.jpg');
+      
+    }
+    #hero h2{
+      color: blue;
+
+    }
+  </style>
+
+  
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+      
+        
+      </div>
+      
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="index.html">Food Menu And Pricing Portal<span>.</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">News</a></li>
+          <li><a class="nav-link scrollto" href="gallery.html">Gallery</a>
+        </li><li><a class="nav-link scrollto" href="offers.php">Offers</a></li>
+          <li><a class="nav-link scrollto" href="adminlogin.php">Admin</a></li>
+		  </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+    <div class="container" data-aos="zoom-out" data-aos-delay="100">
+    <center>
+    <h1>Welcome to Ardhi University <span>Food Menu and Pricing </span> Portal.</h1>
+      <h2>Best taste for your soul</h2>
+      <p>Explore a world of culinary delights with our Food Menus. Whether you're a food enthusiast or a restaurant owner, our platform is designed to enhance your dining experience. Discover a diverse range of dishes, from savory classics to innovative creations, curated to satisfy every palate. Join us on a gastronomic journey where flavor meets convenience, and let Food Menu redefine the way you experience food.</p>
+    </center>
+      <div class="d-flex">
+        <a href="login.php" class="btn-get-started scrollto" href="#GetStarted">Get Started</a>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+     <!-- ======= Get Started Section ======= -->
+    <section id="about" class="GetStarted section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Get Started</h2>
+          <h3>Are You <span>Visitor to our Area?</span></h3>
+          <p>If yes, please visit <span class="text-danger">Playtime</span> immediately!, or you may visit <span class="text-danger">Geti dogo cafteria</span> near Unuversity gate 2.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+            <img src="img/picha.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
+            <h3>* If you have got a meal , please continue.</h3>
+            <ul>
+              <li>
+                <i class="bx bx-store-alt"></i>
+                <div>
+                  <h5>Did you enjoy the meal?</h5>
+                  <p>All stuffs provided around university are measured and qualifies to be provided to the university comunity.</p>
+                </div>
+              </li>
+              <li>
+                <i class="bx bx-images"></i>
+              </li>
+            </ul>
+            <p>
+             Note: On this Official platform u will be able to visit our menus and cafes around university
+            </p>
+			  <div>
+	
+				  
+			  </di>
+          </div>
+        
+    </section><!-- End Team Section -->
+
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
